d5219202b7c8f789e559c8ef4cff077e4d96896e
MaxMendgen <maxmendgen@gmail.com>
2023-04-29 12:53:40 +0200

    QuizHandler 1

---
+++ Assets/Scripts/QuizData/QuizHandler.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class QuizHandler : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField] private TMP_Text _QuestionText;
+    [SerializeField] private Sprite _QuestionImage;
+    [SerializeField] private TMP_InputField _AnswerInputField;
+
+    [Header("Set")]
+    public QuizData quizData;
+    int currentQuestion = 0;
+
+    [Header("Info")]
+    public bool quizFinished = false;
+
+    private void Start()
+    {
+        DisplayQuestion(quizData.questions[currentQuestion]);
+    }
+
+    public void OnClickSubmit()
+    {
+        CheckAnswer();
+        //DisplayCorrectAnswer();
+        currentQuestion++;
+        if (currentQuestion < quizData.questions.Count)
+        {
+            DisplayQuestion(quizData.questions[currentQuestion]);
+        }
+        else
+        {
+            quizFinished = true;
+            print("Quiz ended");
+        }
+    }
+
+    void DisplayQuestion(Question question)
+    {
+        _QuestionText.text = question.question;
+        if (question.image != null)
+            _QuestionImage = question.image;
+
+        //Need to change the input of the answers
+    }
+
+    void CheckAnswer()
+    {
+        switch (quizData.questions[currentQuestion].answerType)
+        {
+            case Question.AnswerType.Text:
+                if (_AnswerInputField.text == quizData.questions[currentQuestion].answers[0])
+                {
+                    print("Correct Answer");
+                }
+                else
+                {
+                    print("Wrong Answer");
+                }
+                break;
+
+            case Question.AnswerType.MultipleChoice:
+                throw new System.NotImplementedException();
+                //break;
+        }
+    }
+}
