889dfba55972076f8ca39fd3b69319ae99e4c82f
MaxMendgen <maxmendgen@gmail.com>
2023-05-15 13:31:32 +0200

    QuizHandler main stuff done

--- Assets/Scripts/QuizData/QuizHandler.cs
+++ Assets/Scripts/QuizData/QuizHandler.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -13,6 +12,7 @@ public class QuizHandler : MonoBehaviour
     [SerializeField] private Sprite _QuestionImage;
     [SerializeField] private GameObject _TextInputArea, _NumberInputArea, _MultipleChoiceInputArea;
     [SerializeField] private TMP_InputField _AnswerTextInputField, _AnswerNumberInputField;
+    [SerializeField] private GameObject _MultipleChoiceButtonPrefab;
 
     [Header("Set")]
     public QuizData quizData;
@@ -20,6 +20,9 @@ public class QuizHandler : MonoBehaviour
 
     [Header("Info")]
     public bool quizFinished = false;
+
+    List<GameObject> multipleChoiceButtons = new List<GameObject>();
+    int multipleChoiceSelected = -1;
 
     private void Start()
     {
@@ -36,9 +39,24 @@ public class QuizHandler : MonoBehaviour
         if (quizFinished)
             return;
 
-        CheckAnswer();
+        switch (CheckAnswer())
+        {
+            case true:
+                print("Correct Answer");
+                break;
+
+            case false:
+                print("Wrong Answer");
+                break;
+
+            case null:
+                return;
+        }
+
         ///DisplayCorrectAnswer();
+        
         currentQuestion++;
+
         if (currentQuestion < quizData.questions.Count)
         {
             DisplayQuestion(quizData.questions[currentQuestion]);
@@ -52,52 +70,71 @@ public class QuizHandler : MonoBehaviour
 
     void DisplayQuestion(Question question)
     {
+        //Text
         _QuestionText.text = question.question;
+
+        //Image
         if (question.image != null)
             _QuestionImage = question.image;
 
-        _TextInputArea.SetActive(quizData.questions[currentQuestion].answerType == Question.AnswerType.Text);
-        _NumberInputArea.SetActive(quizData.questions[currentQuestion].answerType == Question.AnswerType.Number);
-        _MultipleChoiceInputArea.SetActive(quizData.questions[currentQuestion].answerType == Question.AnswerType.MultipleChoice);
+        //Areas
+        _TextInputArea.SetActive(question.answerType == Question.AnswerType.Text);
+        _NumberInputArea.SetActive(question.answerType == Question.AnswerType.Number);
+        bool isMultipleChoice = question.answerType == Question.AnswerType.MultipleChoice;
+        _MultipleChoiceInputArea.SetActive(isMultipleChoice);
+
+        //Clear buttons for multiple choice
+        foreach (GameObject g in multipleChoiceButtons)
+            Destroy(g);
+        multipleChoiceButtons = new List<GameObject>();
+        multipleChoiceSelected = -1;
+
+        //Add new buttons for multiple choice
+        if (isMultipleChoice)
+        {
+            for (int i = 0; i < question.multipleChoiceAnswers.Count; i++)
+            {
+                GameObject newButton = Instantiate(_MultipleChoiceButtonPrefab, _MultipleChoiceInputArea.transform);
+                int selected = i; //Must set new variable
+                newButton.GetComponent<Button>().onClick.AddListener(delegate { OnClickMultipleChoiceButton(selected); });
+                newButton.transform.Find("Text").GetComponent<TMP_Text>().text = question.multipleChoiceAnswers[i].value;
+                multipleChoiceButtons.Add(newButton);
+            }
+        }
     }
 
-    void CheckAnswer()
+    void OnClickMultipleChoiceButton(int selected)
+    {
+        //Disable / Enable Seletion
+        for (int i = 0; i < multipleChoiceButtons.Count; i++)
+            multipleChoiceButtons[i].transform.Find("Selection").GetComponent<CanvasGroup>().alpha = i == selected ? 1 : 0;
+
+        //Set
+        multipleChoiceSelected = selected;
+    }
+
+    bool? CheckAnswer()
     {
         switch (quizData.questions[currentQuestion].answerType)
         {
             case Question.AnswerType.Text:
-                if (quizData.questions[currentQuestion].textAnswers.CheckForText(_AnswerTextInputField.text))
-                {
-                    print("Correct Answer");
-                }
+                if (_AnswerTextInputField.text != "")
+                    return quizData.questions[currentQuestion].textAnswers.CheckForText(_AnswerTextInputField.text);
                 else
-                {
-                    print("Wrong Answer");
-                }
-                break;
+                    return null;
 
             case Question.AnswerType.Number:
-                if (quizData.questions[currentQuestion].numberAnswers.CheckForNumber(float.Parse(_AnswerNumberInputField.text)))
-                {
-                    print("Correct Answer");
-                }
+                if (_AnswerNumberInputField.text != "")
+                    return quizData.questions[currentQuestion].numberAnswers.CheckForNumber(float.Parse(_AnswerNumberInputField.text));
                 else
-                {
-                    print("Wrong Answer");
-                }
-                break;
+                    return null;
 
             case Question.AnswerType.MultipleChoice:
-                throw new System.NotImplementedException("Multiple Choice");
-                if (quizData.questions[currentQuestion].multipleChoiceAnswers.CheckForMultipleChoice(0))
-                {
-                    print("Correct Answer");
-                }
+                if (multipleChoiceSelected > 0)
+                    return quizData.questions[currentQuestion].multipleChoiceAnswers.CheckForMultipleChoice(multipleChoiceSelected);
                 else
-                {
-                    print("Wrong Answer");
-                }
-                break;
+                    return null;
         }
+        return null;
     }
 }
